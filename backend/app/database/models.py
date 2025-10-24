876453a5045dbbee3b264712875e603510f2fbf8
lghuy05 <luonggiahuy1909@gmail.com>
2025-10-24 13:37:57 -0400

    fix backend

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -12,3 +12,4 @@ class User(Base):
     age = Column(Integer)
     sex = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    role = Column(String, default="patient")
--- backend/app/routes/triage.py
+++ backend/app/routes/triage.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from app.schemas.schemas import SymptomInput, TriageOut
 from app.services.triage_service import triage_rules
-from app.routes.auth import user_dependency
-from app.database.database import db_dependency
 
 router = APIRouter()
 
